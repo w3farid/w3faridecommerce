3794356ebc3591ca0d6a3b24c02211e8bf86396f
Farid <farid@tgalimited.com>
2018-07-23 13:24:53 +0600

    first commit

---
+++ src/main/java/com/w3farid/ecommerce/w3faridecommerce/W3faridecommerceApplication.java
@@ -0,0 +1,12 @@
+package com.w3farid.ecommerce.w3faridecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3faridecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3faridecommerceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/w3farid/ecommerce/w3faridecommerce/W3faridecommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.w3farid.ecommerce.w3faridecommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class W3faridecommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
